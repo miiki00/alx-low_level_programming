3339e731ea959b3c7c598a1d0a49c1b24ada703a
Mikias <mikiasabiy16@gmail.com>
2022-09-12 14:05:59 +0000

    Task 11 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - 
+ */
+int main(void)
+{
+	int fir, sec, thi;
+
+	fir = sec = 48;
+
+	while (fir < 56)
+	{
+			sec = fir + 1;
+		
+		while (sec < 57)
+		{
+			thi = sec + 1;
+			 while (thi <= 57)
+			{
+				putchar(fir);
+				putchar(sec);
+				putchar(thi);
+
+				if (fir != 55 || sec != 56 || thi != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				
+				thi++;
+			}
+			
+			sec++;
+		}
+
+		fir++;
+	}
+	putchar('\n');
+	return (0);
+}
+
