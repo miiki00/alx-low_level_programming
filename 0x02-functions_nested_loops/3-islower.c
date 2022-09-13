1133f98d6ca3f7cc87f7a278979c646755be497f
Mikias <mikiasabiy16@gmail.com>
2022-09-13 17:53:11 +0000

    Fix: 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *  _islower - tells if c is lower case or upper case.
- * @c - the character to be evaluated.
+ * @c: the character to be evaluated.
  *
  * Return: 1 if c is lowercase else 0.
  */
@@ -10,10 +10,13 @@ int _islower(int c)
 {
 	int ret;
 
- 	if (c < 122 && c > 97)
+	if (c < 122 && c > 97)
+	{
 		ret = 1;
+	}
 	else
+	{
 		ret = 0;
-
+	}
 	return (ret);
 }
