40c86b2aeeaa7f9bb341ae31bdf6015277cf5298
Mikias <mikiasabiy16@gmail.com>
2022-09-14 15:04:59 -0400

    update: file: main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int);
-
+void print_times_table(int);
+void split_and_print(int, int);
+void print_it(int, int, int, int);
