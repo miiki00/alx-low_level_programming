338df7ce35b82602a81bbf49d0385b81735ab330
Mikias <mikiasabiy16@gmail.com>
2022-09-15 21:03:47 -0400

    second: update: 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,18 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) && (year % 400 == 0) && (year % 100 == 0))
+	int leap;
+
+	if ((year % 4) == 0)
+	{
+		if (year % 100 == 0 && year % 400 == 0)
+			leap = 1;
+		else if (year % 100 != 0)
+			leap = 1;
+		else
+			leap = 0;
+	}
+	if (leap == 1)
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -24,11 +35,9 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 
-		if (month == 2 && day == 60)
+		if (month >= 2 && day >= 60)
 		{
-			day--;
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Invalid date %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
