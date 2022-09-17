c5d7144e2af834565285c60e1913cbdd7d860fdf
Mikias <mikiasabiy16@gmail.com>
2022-09-17 16:55:43 -0400

    second: update: 101-print_number.c third: main.h

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,9 +8,10 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		print_neg(n);
+		n = n * -1;
+		_putchar('-');
 	}
-	else if (n >= 1000)
+	if (n >= 1000)
 	{
 		_putchar((n / 1000) + '0');
 		_putchar(((n % 1000 - n % 100) / 100) + '0');
@@ -26,34 +27,5 @@ void print_number(int n)
 	{
 		_putchar((n / 10) + '0');
 	}
-	if (n >= 0)
-		_putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 }
-
-/**
- * print_neg - print the negative number @n
- * @n: The number to be printed.
- *
- * Return: Void.
- */
- void print_neg(int n)
- {
- 	_putchar('-');
-	if(n <= -1000)
-	{
-		_putchar(((n / 1000) * -1) + '0');
-		_putchar((((n % 1000 - n % 100) / 100) * -1) + '0');
-		_putchar((((n % 100 - n % 10) / 10) * -1) + '0');
-	}
-	else if (n <= -100)
-	{
-		_putchar(((n / 100) * -1) + '0');
-		_putchar((((n % 100 - n % 10) / 10) * -1) + '0');
-	}
-	else if (n <= -10)
-	{
-		_putchar(((n / 10) * -1) + '0');
-	}
-
-		_putchar(((n % 10) * -1)+ '0');
- }
