6d3f6df3658e6df78b8d977b7b6a5f7dddf5a08e
Mikias <mikiasabiy16@gmail.com>
2022-09-19 14:28:33 -0400

    Task 7 done: add: 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - countes the length of a string
+ * @s: Pointer to the first value of the string.
+ *
+ * Return: Void.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts_half - prints after the half of the string
+ * @str: The address of the string to be printed.
+ *
+ * Return: Void.
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = _strlen(str);
+
+	if (len % 2 != 0)
+	{
+		len -= 1;
+	}
+	for (i = (len / 2); i < len; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
