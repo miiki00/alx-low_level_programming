2f130f987a382169be2645e5ae7a65eeea648345
Mikias <mikiasabiy16@gmail.com>
2022-09-22 07:48:42 -0400

    Task 2 done: add: 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies the first @n bytes of string locatd @src to the
+ * first n bytes of the string located @dest.
+ * @dest: The location of the string to copy the string located @src.
+ * @src: The location of the string to be copied to the
+ * array located @dest.
+ * @n: The number of bytes to be copied.
+ *
+ * Return: The adress of the dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, len;
+
+	len = _strlen(src);
+
+	if (n > len)
+	{
+		n = len + 1;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
+
+/**
+ * _strlen - countes the length of a string
+ * @s: Pointer to the first value of the string.
+ *
+ * Return: Void.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
