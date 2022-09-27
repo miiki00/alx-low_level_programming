c0c4de4806a2e024945a22af4ac4af40bad39fa8
Mikias <mikiasabiy16@gmail.com>
2022-09-27 06:35:11 -0400

    second: update: 7-print_chessboar.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,12 +11,12 @@ void print_chessboard(char (*a)[8])
 	int i, j;
 	char h;
 
-	for (i = 0; a[i][0] != 0 ; i++)
+	for (i = 0; i < 8 ; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
 			h = a[i][j];
-			if ((h <= 90 && h >= 65) || (h <= 122 && h >= 97))
+			if (h != ',')
 			{
 				_putchar(a[i][j]);
 			}
