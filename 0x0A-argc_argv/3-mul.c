cf94f702c7c3f04ddaa9a5d9d3df2ce886c07411
miiki00 <mikiasabiy16@gmail.com>
2022-09-30 08:36:06 -0500

    Task 3 done: add: 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies the first two args of main.
+ * @argc: The number of args.
+ * @argv: The arguments.
+ *
+ * Return: The output of the multiplication  else  1.
+ */
+int main(int argc, char *argv[])
+{
+	int ret;
+
+	if  (argc != 3)
+	{
+		ret = 1;
+		printf("Error");
+	}
+	else
+	{
+		ret = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", ret);
+	}
+	return (ret);
+}
