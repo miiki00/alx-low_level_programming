942e0be9a7195b93d048ca2eefcc3f3e0e363364
miiki00 <mikiasabiy16@gmail.com>
2022-09-30 17:33:38 -0500

    seventh: update: 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	if  ((argc = 3) || !(atoi(argv[1]) && atoi(argv[2])))
+	if  (argc != 3 || !(atoi(argv[1]) && atoi(argv[2])))
 	{
 		printf("Error");
 		return (1);
