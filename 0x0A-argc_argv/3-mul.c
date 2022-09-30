cdb10ec192b9b433ae449a21f4415d8e143c5e09
miiki00 <mikiasabiy16@gmail.com>
2022-09-30 17:30:02 -0500

    fifth: update: 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,6 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		printf("%d\n", atoi(argv[1]));
-		return (0);
-	}
 	if  ((argc <= 3) || !(atoi(argv[1]) && atoi(argv[2])))
 	{
 		printf("Error");
