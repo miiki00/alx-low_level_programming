5e9f6c41df83861f624e4e07ef9ad46adafbcbe9
Mikias <mikiasabiy16@gmail.com>
2022-10-05 10:02:28 +0000

    Task 5 done: add: 100-argstostr.c update: main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,7 +8,9 @@ char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
-void free_grid(int **grid, int height);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+char **strtow(char *str);
 
 
 #endif
