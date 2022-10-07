54a4dba29dd7eb0ce1424f3794d42e573312d634
Mikias <mikiasabiy16@gmail.com>
2022-10-07 08:41:49 +0000

    Task 1 done: add: 1-string_nconcat.c update: main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int);
-char *string_noncat(char *, char *, unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
 
