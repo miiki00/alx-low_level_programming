848f2136471a12859c119c7f43eb4cd99b956ac3
Mikias <mikiasabiy16@gmail.com>
2022-10-07 07:36:52 +0000

    Task 0 done: add: 0-malloc_checked.c, main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int);
+char *string_noncat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+
+
+#endif
