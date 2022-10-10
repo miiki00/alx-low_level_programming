416b4f8806ba35a4d7d70105636dd9b7034f6133
mikias <mikiasabiy16@gmail.com>
2022-10-10 16:09:11 -0400

    Task 1 done: add: 1-init_dog.c update: dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *, char *, float, char *);
+void print_dog(struct dog *);
+dog_t *new_dog(char *, float age, char *owner);
+void free_dog(dog_t *);
+
+
 #endif
