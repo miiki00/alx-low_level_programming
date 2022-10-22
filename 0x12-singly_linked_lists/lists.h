0d3e46609bfed63b01e2332537e4b656751be877
mikias <mikiasabiy16@gmail.com>
2022-10-22 12:09:44 -0400

    update: lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,7 +24,7 @@ int _putchar(char);
 size_t print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *);
-list_t *add_nod_end(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
 void free_list(list_t *);
 
 #endif
