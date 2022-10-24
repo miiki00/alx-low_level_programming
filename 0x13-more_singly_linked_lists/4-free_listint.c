3feb4d797c97fc68d1015ba734f3e195d2a3fe6c
mikias <mikiasabiy16@gmail.com>
2022-10-24 06:57:04 -0400

    Task 4 done: 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a memory occupied by a list kind of
+ * listint_t.
+ * @head: The head of the list.
+ *
+ * Return: Void.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp = NULL, *tmp2 = NULL;
+
+	for (tmp = head, tmp2 = head->next; tmp2 != NULL; tmp = tmp2)
+	{
+		tmp2 = tmp->next;
+		free(tmp);
+	}
+}
