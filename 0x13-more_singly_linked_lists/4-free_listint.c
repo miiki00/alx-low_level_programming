33d9f64beb116b6fec1d46bf726a20df0271e3f0
mikias <mikiasabiy16@gmail.com>
2022-10-24 08:25:48 -0400

    update: 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,6 +11,8 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp = NULL, *tmp2 = NULL;
 
+	if (head == NULL)
+		return;
 	for (tmp = head, tmp2 = head->next; tmp2 != NULL; tmp = tmp2)
 	{
 		tmp2 = tmp->next;
