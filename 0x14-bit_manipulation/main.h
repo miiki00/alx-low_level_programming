e5eeaf7e98576c2921c877835ea64f9d998d2943
mikias <mikiasabiy16@gmail.com>
2022-10-28 19:10:29 -0400

    update: main.h: prototype defination mistake fixed

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,7 +8,7 @@ int _putchar(char);
 unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
-int set_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
 int clear_bit(unsigned long int, unsigned int);
 unsigned int flip_bits(unsigned long int n, unsigned long int);
 
