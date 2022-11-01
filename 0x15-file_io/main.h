3cb2b0c9f57a8728e9d211a7dd2196841c40c1df
mikias <mikiasabiy16@gmail.com>
2022-11-01 13:59:13 -0400

    add: main.h: Header file for the whole .c files in this directory

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *, size_t);
+int creat_file(const char *, char *);
+int append_text_to_file(const char *, char *);
+
+#endif
