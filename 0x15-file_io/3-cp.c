1d2d79606b151b02f1755a244b34b6ecf73f01fb
mikias <mikiasabiy16@gmail.com>
2022-11-03 07:04:12 -0400

    update: 3-cp.c: whole code base changed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,50 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text to a file.
- * Description: If the file does not exist it will not creat the file.
- * @filename: The name of the file.
- * @text_content: The content you went to append.
+ * free_and_close - fress memory allocated by malloc and closes any file
+ * descriptor that is opened by the program.
+ * @size_fd: The number of arguments passed int the valist.
+ * @list: The va_list that contain the arguments.
  *
- * Return: The number of bytes appended, -1 on error.
+ * Return: Void.
  */
-int append_text_to_file(const char *filename, char *text_content)
+void free_and_close(int size_fd, va_list list)
 {
-	ssize_t ret, len;
-	int fd;
+	int i, fd, ret;
 
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
+	for (i = 0; i <= size_fd; i++)
 	{
-		len = strlen(text_content);
-		ret = write(fd, text_content, len);
-		if (ret != len)
+		if (i == size_fd && size_fd == 2)
+			free(va_arg(list, char *));
+		if (i != size_fd)
 		{
-			close(fd);
-			return (-1);
+			fd = (va_arg(list, int));
+			ret = close(fd);
+			if (ret == -1)
+				_err_code(0, 100, &fd);
 		}
 	}
-	close(fd);
-	return (1);
-}
 
-/**
- * create_file - creates a file with name @filename and content
- * form @text_content.
- * Descritption: If the file exists it will be repalced with the one
- * you created. If the content is empty it will creat an empty file.
- * @filename: The name of the file to be created.
- * @text_content: The content of the file.
- *
- * Return: 1 on success -1 on error.
- */
-int create_file(const char *filename, char *text_content)
-{
-	int len, fd;
-	ssize_t ret;
-
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		len = strlen(text_content);
-		ret = write(fd, text_content, len);
-		if (ret != len)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	ret = close(fd);
-	if (ret == -1)
-		_err_code(100, &fd);
-	return (1);
 }
 
 /**
  * _err_code - prints an error message correspondig to the error
  * code given and exit with the give code status.
+ * @size_fd: The number of valist args passed.
  * @code: The error code.
  * @data: The name of the data that caused the error.
- *
  * Return: Void.
  */
-void _err_code(int code, const void *data)
+void _err_code(int size_fd, int code, const void *data, ...)
 {
+	va_list list;
+
+	if (size_fd != 0)
+	{
+		va_start(list, data);
+		free_and_close(size_fd, list);
+		va_end(list);
+	}
 	switch (code)
 	{
 		case 97:
@@ -96,68 +63,39 @@ void _err_code(int code, const void *data)
 }
 
 /**
- * copy_from_to - copies text from the file @from to the file @to.
- * @from: The file to copy form.
- * @to: The fiel to copy to.
+ * main - a program that copies a content of a file to another file.
+ * @ac: The number of arguments.
+ * @av: The actual arguments array of strings.
  *
- * Return: 1 on success -1 on error.
- */
-int copy_from_to(const char *from, const char *to)
-{
-	int fd_from;
-	ssize_t readed, ret;
-	char *buf = NULL;
-
-	fd_from = open(from, O_RDONLY);
-	if (fd_from == -1)
-		_err_code(98, from);
-	ret = create_file(to, NULL);
-	if (ret == -1)
-		_err_code(99, to);
-	do {
-		buf = malloc(sizeof(char) * BUFF_SIZE);
-		if (buf == NULL)
-			return (-1);
-		readed = read(fd_from, buf, BUFF_SIZE);
-		if (readed == -1)
-		{
-			ret = close(fd_from);
-			if (ret == -1)
-				_err_code(100, &fd_from);
-			free(buf);
-			_err_code(98, from);
-		}
-		ret = append_text_to_file(to, buf);
-		if (ret == -1)
-		{
-			ret = close(fd_from);
-			if (ret == -1)
-				_err_code(100, &fd_from);
-			free(buf);
-			_err_code(99, to);
-		}
-		free(buf);
-		buf = NULL;
-	} while (readed == BUFF_SIZE);
-	return (1);
-}
-
-/**
- * main - A program that copies content of a text file
- * to another text file based on given parameters.
- * @ac: The Number of arguments.
- * @av: Actual arguments.
- *
- * Return: on success 0 on error number greater than 0.
+ * Return: 0 on success, 1 on error.
  */
 int main(int ac, char **av)
 {
-	int ret;
+	int fd_from, fd_to;
+	ssize_t readed, ret;
+	char *buf = NULL;
 
 	if (ac != 3)
-		_err_code(97, av[0]);
-	ret = copy_from_to(av[1], av[2]);
-	if (ret == -1)
-		return (1);
+		_err_code(0, 97, NULL);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		_err_code(0, 98, av[1]);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
+	if (fd_to == -1)
+		_err_code(1, 99, av[2], fd_from);
+	do {
+		buf = malloc(sizeof(char) * BUFF_SIZE);
+		if (buf == NULL)
+		{
+			_err_code(2, 0, NULL, fd_from, fd_to);
+			return (1);
+		}
+		readed = read(fd_from, buf, BUFF_SIZE);
+		if (readed == -1)
+			_err_code(2, 98, av[1], fd_from, fd_to, buf);
+		ret = write(fd_to, buf, readed);
+		if (ret != readed)
+			_err_code(2, 99, av[2], fd_from, fd_to, buf);
+	} while (readed == BUFF_SIZE);
 	return (0);
 }
