54f2d3610d37a3d7650bd557fd1d41758f918ef7
mikias <mikiasabiy16@gmail.com>
2022-12-22 04:02:24 -0500

    update: 6-is_prime_number.c: implementation changed to use recursion instead of loops

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * is_prime_number - checks is n is prime number or not.
@@ -8,17 +9,31 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int ret;
 
 	if (n == 2 || n == 3)
 		return (1);
-	if (n % 2 == 0 || n % 3 == 0 || n <= 1)
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n <= 1)
 		return (0);
-	i = 5;
-	do {
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-		i = i + 6;
-	} while (i * i < n);
-	return (1);
+	ret = check_the_rest(n, 5);
+	return (ret);
+}
+
+/**
+ * check_the_rest - checks if n is prime or not using recursion.
+ * @n: The number to check.
+ * @start: The number to start checking if n divisiblity from.
+ *
+ * Return: 1 (n is prime), 0 (n is not prime)
+ */
+int check_the_rest(int n, int start)
+{
+	int ret;
+
+	if (start * start >= n)
+		return (1);
+	if (n % start == 0 || n % (start + 2) == 0)
+		return (0);
+	ret = check_the_rest(n, start + 6);
+	return (ret);
 }
